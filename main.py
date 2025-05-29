2b3b37805473b17c042bbfdfaf98af5bdca4a2a9
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-05-29 14:14:04 +0900

    ⚗️ discord-bot test

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from discord-bot!")
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot_token = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 
-if __name__ == "__main__":
-    main()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello world!")
+
+
+client.run(bot_token)
